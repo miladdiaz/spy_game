40af7e0f010bccfebc5b5345080955b89c26ccc2
miladdiaz <miladmomeni@gmail.com>
2024-08-19 16:30:09 +0330

    add button widget

---
+++ lib/widgets/button.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class Button extends StatelessWidget {
+  final void Function() onPressed;
+  final String label;
+  final Color? color;
+
+  const Button({
+    super.key,
+    required this.onPressed,
+    required this.label,
+    this.color = Colors.blue,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: color,
+        padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+      ),
+      onPressed: onPressed,
+      child: Text(
+        label,
+        style: const TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+}
