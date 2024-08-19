ddc17cbe204b8aadba088072dba0401c02f74c4c
miladdiaz <miladmomeni@gmail.com>
2024-08-19 16:31:18 +0330

    remove unnecessary

--- lib/widgets/counter.dart
+++ lib/widgets/counter.dart
@@ -5,7 +5,6 @@ class Counter extends StatelessWidget {
   final String? label;
   final int count;
   final int startFrom;
-  // on change function pass new count value
   final void Function(int value) onChange;
 
   const Counter({
@@ -46,17 +45,19 @@ class Counter extends StatelessWidget {
                 onChange(playerCount[index]);
               },
               children: playerCount
-                  .map((e) => Align(
-                        alignment: Alignment.center,
-                        child: Text(
-                          '$e',
-                          textAlign: TextAlign.start,
-                          style: const TextStyle(
-                              fontSize: 24,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white),
-                        ),
-                      ))
+                  .map(
+                    (e) => Align(
+                      alignment: Alignment.center,
+                      child: Text(
+                        '$e',
+                        textAlign: TextAlign.start,
+                        style: const TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white),
+                      ),
+                    ),
+                  )
                   .toList(),
             ),
           ),
@@ -65,82 +66,3 @@ class Counter extends StatelessWidget {
     );
   }
 }
-
-// import 'package:flutter/cupertino.dart';
-// import 'package:flutter/material.dart';
-
-// class Counter extends StatefulWidget {
-//   final String? label;
-//   final int count;
-//   final int startFrom;
-//  void Function(int value) onChange;
-
-//   const Counter({
-//     super.key,
-//     this.label,
-//     this.startFrom = 0,
-//     required this.count,
-//     required this.onChange,
-//   });
-
-//   @override
-//   State<Counter> createState() => _CounterState();
-// }
-
-// class _CounterState extends State<Counter> {
-//   List<int> _playerCount = [];
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     setState(() {
-//       _playerCount =
-//           List.generate(widget.count, (index) => (index + widget.startFrom));
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Column(
-//       children: [
-//         Text(
-//           widget.label!,
-//           style: const TextStyle(
-//             fontSize: 24,
-//             fontWeight: FontWeight.bold,
-//           ),
-//         ),
-//         SizedBox(
-//           height: 150,
-//           width: MediaQuery.of(context).size.width * 0.25,
-//           child: Container(
-//             decoration: BoxDecoration(
-//               color: Theme.of(context).colorScheme.primary.withOpacity(0.8),
-//               borderRadius: BorderRadius.circular(10),
-//             ),
-//             child: CupertinoPicker(
-//               magnification: 1.3,
-//               squeeze: 1.2,
-//               itemExtent: 48,
-//               diameterRatio: 2.1,
-//               onSelectedItemChanged: (int index) {},
-//               children: _playerCount
-//                   .map((e) => Align(
-//                         alignment: Alignment.center,
-//                         child: Text(
-//                           '$e',
-//                           textAlign: TextAlign.start,
-//                           style: const TextStyle(
-//                               fontSize: 24,
-//                               fontWeight: FontWeight.bold,
-//                               color: Colors.white),
-//                         ),
-//                       ))
-//                   .toList(),
-//             ),
-//           ),
-//         ),
-//       ],
-//     );
-//   }
-// }
