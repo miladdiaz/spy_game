40845a5363548e40d76a4d4afbafd8d9649d6d7e
miladdiaz <miladmomeni@gmail.com>
2024-08-24 22:30:23 +0330

    add join the game container

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -16,54 +16,104 @@ class HomeScreen extends ConsumerWidget {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 30, 13, 63),
       body: SafeArea(
-        child: Container(
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage("assets/images/dark_house.png"),
-              fit: BoxFit.contain,
-              alignment: Alignment.bottomCenter,
+        child: SingleChildScrollView(
+          reverse: true,
+          child: Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/images/dark_house.png"),
+                fit: BoxFit.cover,
+                alignment: Alignment.bottomCenter,
+              ),
             ),
-          ),
-          child: Column(
-            children: [
-              const SizedBox(height: 16),
-              const Logo(),
-              const SizedBox(height: 64),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Counter(
-                    label: 'Spy',
-                    count: 4,
-                    startFrom: 1,
-                    onChange: gameNotifier.setSpyCount,
+            child: Column(
+              children: [
+                const SizedBox(height: 16),
+                const Logo(),
+                const SizedBox(height: 64),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Counter(
+                      label: 'Spy',
+                      count: 4,
+                      startFrom: 1,
+                      onChange: gameNotifier.setSpyCount,
+                    ),
+                    Counter(
+                      label: 'Citizens',
+                      count: 30,
+                      startFrom: 2,
+                      onChange: gameNotifier.setCitizenCount,
+                    ),
+                    Counter(
+                      label: 'Time',
+                      count: 30,
+                      startFrom: 1,
+                      onChange: gameNotifier.setGameTime,
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 64),
+                Button(
+                  label:
+                      'Start with ${game.spyCount} Spy & ${game.citizenCount} citizens',
+                  onPressed: () {
+                    gameNotifier.startGame().then((value) {
+                      Navigator.pushNamed(context, '/play');
+                    });
+                  },
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.8,
+                  margin: const EdgeInsets.symmetric(vertical: 16),
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 16,
+                    vertical: 16,
                   ),
-                  Counter(
-                    label: 'Citizens',
-                    count: 30,
-                    startFrom: 2,
-                    onChange: gameNotifier.setCitizenCount,
+                  decoration: BoxDecoration(
+                    // color: Colors.white.withOpacity(0.3),
+                    border: Border.all(
+                        color: Colors.white.withOpacity(0.5), width: 2),
+                    borderRadius: BorderRadius.circular(16),
                   ),
-                  Counter(
-                    label: 'Time',
-                    count: 30,
-                    startFrom: 1,
-                    onChange: gameNotifier.setGameTime,
+                  child: Column(
+                    children: [
+                      TextField(
+                        keyboardType: TextInputType.text,
+                        decoration: InputDecoration(
+                          labelText: 'Enter token',
+                          labelStyle: const TextStyle(color: Colors.white),
+                          border: const OutlineInputBorder(),
+                          focusedBorder: const OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white),
+                          ),
+                          enabledBorder: const OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white),
+                          ),
+                          disabledBorder: const OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white),
+                          ),
+                          filled: true,
+                          fillColor: Colors.white.withOpacity(0.1231),
+                        ),
+                        onChanged: (value) {},
+                      ),
+                      const SizedBox(height: 16),
+                      Button(
+                        label: 'Join a game',
+                        onPressed: () {
+                          gameNotifier.startGame().then((value) {
+                            // join the game
+                          });
+                        },
+                      ),
+                    ],
                   ),
-                ],
-              ),
-              const SizedBox(height: 64),
-              Button(
-                label:
-                    'Start with ${game.spyCount} Spy & ${game.citizenCount} citizens',
-                onPressed: () {
-                  gameNotifier.startGame().then((value) {
-                    Navigator.pushNamed(context, '/play');
-                  });
-                },
-              ),
-            ],
+                )
+              ],
+            ),
           ),
         ),
       ),
