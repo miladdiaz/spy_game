bb31a6f29095958646246ede5ecfab3fe5b0bf3b
miladdiaz <miladmomeni@gmail.com>
2024-09-25 17:15:57 +0330

    add firebase and firestore

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:spy_game/firebase_options.dart';
 import 'package:spy_game/screens/home_screen.dart';
 import 'package:spy_game/screens/multi_device_screen.dart';
 import 'package:spy_game/screens/play_screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const ProviderScope(child: MyApp()));
 }
 
